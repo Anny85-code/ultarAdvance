19bf8f403f04f6fc0485bde372889f89bc2a4395
uae <aniekaudo@yahoo.com>
2023-03-31 11:16:14 +0100

    :art: Path color
    Update  path color

--- src/components/ServiceHome/ServiceHome.js
+++ src/components/ServiceHome/ServiceHome.js
@@ -5,18 +5,23 @@ import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 
 const ServiceHome = (props) => {
     return (
-        <div className={`${styles.box} user-select-none`}>
-            <img style={{ height: '100px', width: '100px'}} src={props.icon} alt="" className={styles.img} />
-            <p className={styles.heading}>{props.heading}</p>
-            <p className={styles.text}>{props.content}</p>
-            <Link to={`/individualService?id=${props.id}`}>
-                <HiOutlineArrowNarrowRight
-                    className={styles.to}
-                    size="2rem"
-                    color="#ff4c0a"
-                />
-            </Link>
-        </div>
+      <div className={`${styles.box} user-select-none`}>
+        <img
+          style={{ height: '100px', width: '100px' }}
+          src={props.icon}
+          alt=""
+          className={styles.img}
+        />
+        <p className={styles.heading}>{props.heading}</p>
+        <p className={styles.text}>{props.content}</p>
+        <Link to={`/individualService?id=${props.id}`}>
+          <HiOutlineArrowNarrowRight
+            className={styles.to}
+            size="2rem"
+            color="#2B3467"
+          />
+        </Link>
+      </div>
     );
 };
 export default ServiceHome;
