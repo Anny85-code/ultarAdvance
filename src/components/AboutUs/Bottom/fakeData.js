1d7105546830031c7b358ff63cb62cf48ea16129
uae <aniekaudo@yahoo.com>
2023-04-03 22:17:07 +0100

    :art: about us
    Update about us mobile

--- src/components/AboutUs/Bottom/fakeData.js
+++ src/components/AboutUs/Bottom/fakeData.js
@@ -1,7 +1,7 @@
 import img1 from '../../../assets/aboutUs/bottom1.png';
 import img2 from '../../../assets/aboutUs/bottom2.png';
 import img3 from '../../../assets/aboutUs/aboutusmobile2.jpeg';
-import img4 from '../../../assets/aboutUs/aboutusmobile3.jpeg';
+import img4 from '../../../assets/aboutUs/aboutusmobile3.jpg';
 
 const fakeData = [
   {
