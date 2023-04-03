4aced1af8d2fd59d7fd0a4de5a0d1a55ec399377
uae <aniekaudo@yahoo.com>
2023-04-03 22:10:03 +0100

    :bug: About us mobile
    Update abvout us images for mobile

--- src/components/AboutUs/Bottom/Bottom.js
+++ src/components/AboutUs/Bottom/Bottom.js
@@ -3,6 +3,7 @@ import styles from './Bottom.module.css';
 import fakeData from './fakeData';
 
 const Bottom = () =>{
+    const windowSize = window.innerWidth;
     return(
         <>
             <div className={`container ${styles.contain} overflow-hidden`}>
@@ -15,13 +16,21 @@ const Bottom = () =>{
                                 <p className={styles.content}>{x.content}</p>
                             </div>
                             <div data-aos='slide-left' className='col-md-6'>
-                                <img src={x.img} alt='' className={styles.img}/>
+                                {windowSize > 450?
+                                <img src={x.img} 
+                                alt='' className={styles.img}/> :
+                                <img src={x.img1} 
+                                alt='' className={styles.img1}/>
+}
                             </div>
                         </div>
                         :
                         <div key={x.id} className={`row ${styles.row}`}>
                             <div data-aos='slide-right' data-aos-offset="220" className='col-md-6'>
-                                <img src={x.img} alt='' className={styles.img1}/>
+                                {windowSize > 450?
+                                <img src={x.img2} alt='' className={styles.img2}/> :
+                                <img src={x.img3} alt='' className={styles.img3}/>
+}
                             </div>
                             <div data-aos='slide-left' className='col-md-6'>
                                 <p className={styles.head1}>{x.heading}</p>
--- src/components/AboutUs/Bottom/fakeData.js
+++ src/components/AboutUs/Bottom/fakeData.js
@@ -1,18 +1,22 @@
 import img1 from '../../../assets/aboutUs/bottom1.png';
 import img2 from '../../../assets/aboutUs/bottom2.png';
+import img3 from '../../../assets/aboutUs/aboutusmobile2.jpeg';
+import img4 from '../../../assets/aboutUs/aboutusmobile3.jpeg';
 
 const fakeData = [
   {
     id: 1,
     heading: 'We Do with Creativity',
     img: img1,
+    img1: img3,
     content:
       'Sooner or later, every business faces growth difficulties: drop in sales, stagnation, an increasing number of competitors, changes in demand, etc. Most of these challenges can be solved with marketing. A good marketing strategy identifies the problem accurately and forms an exact solution with a step-by-step implementation program. When planning these steps, we take into account the readiness of the company for its financial and organizational changes.',
   },
   {
     id: 2,
     heading: 'We Do with Creativity',
-    img: img2,
+    img2: img2,
+    img3: img4,
     content:
       'A provider of managed office solutions and technology, Document Solutions features a modern, dynamic and fresh website design perfectly reflects our client’s brand image and resonates with a relevant audience.Dynamic modules help keep large amounts of information structured flawlessly to avoid overwhelming consumers with large paragraphs of text and ease their decision-making process.One of the most notable features our web design team implemented on the Document Solutions website is the strategic use of color, with each of the company’s services and service pages differentiated by a specific shade.This not only contributes to the website’s aesthetic appeal, but also simplifies the user journey by making it more visual.',
   },
